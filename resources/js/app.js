535575418ebc82d3608ad03aaead26d9d368a482
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2025-04-30 10:47:41 +0700

    feat: jquery-validation, ignore color-modes error

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,38 @@
-import './bootstrap';
+import "./bootstrap";
 // Simplebar
-import 'simplebar/dist/simplebar.min.js';
+import "simplebar/dist/simplebar.min.js";
 
 // CoreUI
-import '@coreui/chartjs/dist/js/coreui-chartjs.js';
-import '@coreui/utils/dist/umd/index.js';
-import './CoreUI/config.js';
-import './CoreUI/color-modes.js';
-import * as coreui from '@coreui/coreui'
+import "@coreui/chartjs/dist/js/coreui-chartjs.js";
+import "@coreui/utils/dist/umd/index.js";
+import "./CoreUI/config.js";
+import "./CoreUI/color-modes.js";
+import * as coreui from "@coreui/coreui";
 // @ts-ignore
-window.coreui = coreui
+window.coreui = coreui;
 
 // CSRF token setup
-import $ from 'jquery';
+import $ from "jquery";
+import 'jquery-validation';
+// @ts-ignore
+window.$ = $;
 $.ajaxSetup({
     headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    }
+        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+    },
 });
 
 // DataTables
-import dt from 'datatables.net-bs5';
-import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
+import dt from "datatables.net-bs5";
+import "datatables.net-bs5/css/dataTables.bootstrap5.min.css";
+
+const header = document.querySelector("header.header");
+document.addEventListener("scroll", () => {
+    if (header) {
+        header.classList.toggle(
+            "shadow-sm",
+            document.documentElement.scrollTop > 0
+        );
+    }
+});
+
