b86c2caef8ed48eb08a4d5828c820703b788dd80
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2025-05-04 09:34:58 +0700

    vite: add auth.css

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/auth.css",
+            ],
             refresh: true,
         }),
         viteStaticCopy({
@@ -22,10 +26,7 @@ export default defineConfig({
                 },
                 {
                     src: normalizePath(
-                        resolve(
-                            __dirname,
-                            "./node_modules/chart.js/dist/*"
-                        )
+                        resolve(__dirname, "./node_modules/chart.js/dist/*")
                     ),
                     dest: "chart.js",
                 },
