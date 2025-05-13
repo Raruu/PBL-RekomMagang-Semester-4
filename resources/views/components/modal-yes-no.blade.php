ca3968cf78813ee70123eb636137456debbfc474
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2025-05-13 16:41:37 +0700

    props

--- resources/views/components/modal-yes-no.blade.php
+++ resources/views/components/modal-yes-no.blade.php
@@ -1,16 +1,19 @@
-<div class="modal fade" id="modal-yes-no" tabindex="-1">
+@props(['id' => 'modal-yes-no', 'idTrue' => 'btn-true-yes-no', 'title' => 'Yakin?', 'message' => ''])
+
+<div class="modal fade" id="{{ $id }}" tabindex="-1">
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Yakin?</h5>
+                <h5 class="modal-title">{{ $title }}</h5>
                 <button type="button" class="btn-close" data-coreui-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
+                {{ $message }}
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-coreui-dismiss="modal">Batal</button>
                 <button type="button" class="btn btn-primary" data-coreui-dismiss="modal"
-                    id="btn-true-yes-no">Ya</button>
+                    id="{{ $idTrue }}">Ya</button>
             </div>
         </div>
     </div>
