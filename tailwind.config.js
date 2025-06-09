4690b7e79f8e744dd6292281fef37f0999ec878c
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2025-06-09 18:47:59 +0700

    change tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
+        // "./resources/**/*.blade.php",
+        "./resources/views/landing.blade.php",
+        // "./resources/**/*.js",
+        // "./resources/**/*.vue",
     ],
     theme: {
         extend: {},
