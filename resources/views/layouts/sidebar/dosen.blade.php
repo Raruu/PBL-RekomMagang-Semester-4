ed759b9fd21b1540dbb82d641730e7625ca9adf2
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2025-06-10 15:33:51 +0700

    center pfp

--- resources/views/layouts/sidebar/dosen.blade.php
+++ resources/views/layouts/sidebar/dosen.blade.php
@@ -1,25 +1,16 @@
 <li class="nav-title">Manajemen Akun & Profil</li>
-<li class="nav-group">
-    <a class="nav-link nav-group-toggle" href="#">
+<li class="nav-item">
+    <a class="nav-link" href="{{ route('dosen.profile') }}">
         <svg class="nav-icon">
             <use xlink:href="{{ url('build/@coreui/icons/sprites/free.svg#cil-user') }}">
             </use>
-        </svg>
-        Akun
+        </svg> Profil Akun
     </a>
-    <ul class="nav-group-items compact">
-        <li class="nav-item">
-            <a class="nav-link" href="{{ url('dosen/profile') }}">
-                <span class="nav-icon">
-                    <span class="nav-icon-bullet"></span>
-                </span> Profil
-            </a>
-    </ul>
 </li>
 
 <li class="nav-title">Manajemen</li>
 <li class="nav-item">
-    <a class="nav-link" href="{{url ("dosen/mahasiswabimbingan")}}">
+    <a class="nav-link" href="{{ route('dosen.mahasiswabimbingan') }}">
         <svg class="nav-icon">
             <use xlink:href="{{ url('build/@coreui/icons/sprites/free.svg#cil-people') }}">
             </use>
